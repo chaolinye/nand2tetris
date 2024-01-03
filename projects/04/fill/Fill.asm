d16c50c58ee3f1e0637c1288039eb6b88ea6fa2a
yechaolin <2285915269@qq.com>
2024-01-04 00:07:17 +0800

    update

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,47 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(start)
+@SCREEN
+D=A
+@R0
+M=D
+(monitor)
+@KBD
+D=M
+@clean
+D;JEQ
+@0
+D=!A
+@R0
+A=M
+M=D
+D=A+1
+@R0
+M=D
+@KBD
+D=A-D
+@start
+D;JEQ
+@monitor
+0;JMP
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen
-// by writing 'black' in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen by writing
-// 'white' in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-//// Replace this comment with your code.
+(clean)
+@0
+D=A
+@R0
+A=M
+M=D
+D=A-1
+@R0
+M=D
+@SCREEN
+D=D-A
+@cleanstart
+D;JLT
+@monitor
+0;JMP
+(cleanstart)
+@KBD
+D=A-1
+@R0
+M=D
+@monitor
+0;JMP
